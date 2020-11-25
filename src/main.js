8336d9ae828026c380718e7b135dad2855cc4e69
leechikit <leechikit0823@163.com>
2020-11-25 15:11:35 +0800

    fix: 去除init

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
  * @Autor: Lizijie
  * @Date: 2020-07-25 15:14:53
  * @LastEditors: Lizijie
- * @LastEditTime: 2020-11-09 17:58:45
+ * @LastEditTime: 2020-11-10 16:14:28
  */
 import Vue from 'vue'
 import App from './App.vue'
@@ -11,7 +11,6 @@ import router from './router'
 import store from './store'
 import Karrot from 'karrot'
 
-Karrot.init(process.env.PROJECT_ENV)
 console.log(process.env.PROJECT_ENV)
 Vue.config.productionTip = false
 Vue.use(Karrot)
